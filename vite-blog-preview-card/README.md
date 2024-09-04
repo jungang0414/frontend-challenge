c94869239968ed969ef81c646b06376179c3c03f
jungang0414 <dayr88520@gmail.com>
2024-09-04 14:21:34 +0800

    feat: finish challenge

--- vite-blog-preview-card/README.md
+++ vite-blog-preview-card/README.md
@@ -1,8 +1,37 @@
-# React + Vite
+# Frontend Mentor - Blog preview card solution
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a solution to the [Blog preview card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-Currently, two official plugins are available:
+## Overview 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+### The challenge 挑戰
+
+Users should be able to:
+
+- See hover and focus states for all interactive elements on the page
+
+使用者能夠:
+
+- 查看頁面上所有互動元素的懸停和焦點狀態
+
+### Screenshot
+
+![Desktop](/previews.png)
+![Mobile](/previews-m.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://github.com/jungang0414/frontend-challenge/tree/main/vite-blog-preview-card)
+- Live Site URL: [Add live site URL here](https://frontend-blog-preview-challenge.netlify.app/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- vite
+- React
+
+
--- vite-blog-preview-card/src/App.jsx
+++ vite-blog-preview-card/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="container">
+        <div className="card">
+          <img src="/illustration-article.svg" alt="illustration-article" />
+          <div className="card-text">
+            <p className="text-preset-3">
+              <span className="learning text-preset-3-b">Learning</span>
+              <br />
+              Published 21 Dec 2023
+            </p>
+            <h1 className="text-preset-1 active">HTML & CSS foundations</h1>
+            <p className="text-preset-2">
+              These languages are the backbone of every website, defining
+              structure, content, and presentation.
+            </p>
+          </div>
+          <div className="card-footer">
+            <img src="/image-avatar.webp" alt="image-avatar" className="avatar" />
+            <p>Greg Hooper</p>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
