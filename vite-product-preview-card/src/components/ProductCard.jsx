094bc5f3afb0915340d87020e0573ea173a54597
jungang0414 <dayr88520@gmail.com>
2024-09-12 18:39:11 +0800

    feat: update desktop design css 撰寫 桌面版SCSS樣式

--- vite-product-preview-card/src/components/ProductCard.jsx
+++ vite-product-preview-card/src/components/ProductCard.jsx
@@ -6,23 +6,23 @@ const ProductCard = () => {
       <div className="container">
         <div className="img"></div>
         <div className="card-info">
-          <div className="card-header">Perfume</div>
-          <div className="card-title">
+          <div className="card-header montserrat-500">PERFUME</div>
+          <div className="card-title text-preset-1 fraunces-700">
             <h1>Gabrielle Essence Eau De Parfum</h1>
           </div>
-          <div className="card-text">
+          <div className="card-text text-preset-2 montserrat-500">
             <p>
               A floral, solar and voluptuous interpretation composed by Olivier
               Polge, Perfumer-Creator for the House of CHANEL.
             </p>
           </div>
           <div className="card-price">
-            <div className="first-price">$149.99</div>
-            <div className="second-price">$169.99</div>
+            <div className="first-price fraunces-700">$149.99</div>
+            <div className="second-price montserrat-500">$169.99</div>
           </div>
           <div className="card-footer">
             <div className="btn-img"></div>
-            <div className="btn-text">
+            <div className="btn-text montserrat-700">
               <p>Add to Cart</p>
             </div>
           </div>
