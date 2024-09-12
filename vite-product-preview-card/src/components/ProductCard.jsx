a5b12fb8af1c3ae90cd0d7e10933d224fc4c4e16
jungang0414 <dayr88520@gmail.com>
2024-09-12 17:11:47 +0800

    feat: update product html

--- vite-product-preview-card/src/components/ProductCard.jsx
+++ vite-product-preview-card/src/components/ProductCard.jsx
@@ -1,10 +1,30 @@
 const ProductCard = () => {
   return (
     <>
-      <div>
-        Perfume Gabrielle Essence Eau De Parfum A floral, solar and voluptuous
-        interpretation composed by Olivier Polge, Perfumer-Creator for the House
-        of CHANEL. $149.99 $169.99 Add to Cart
+      <div className="container">
+        <div className="img"></div>
+        <div className="card-info">
+          <div className="card-header">Perfume</div>
+          <div className="card-title">
+            <h1>Gabrielle Essence Eau De Parfum</h1>
+          </div>
+          <div className="card-text">
+            <p>
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, Perfumer-Creator for the House of CHANEL.
+            </p>
+          </div>
+          <div className="card-price">
+            <div className="first-price">$149.99</div>
+            <div className="second-price">$169.99</div>
+          </div>
+          <div className="card-footer">
+            <div className="btn-img"></div>
+            <div className="btn-text">
+              <p>Add to Cart</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
