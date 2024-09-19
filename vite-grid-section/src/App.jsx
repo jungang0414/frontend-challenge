08e378a04f4f9146f7fdf655de767f340cbe22c2
jungang0414 <dayr88520@gmail.com>
2024-09-19 18:33:24 +0800

    initial: first commit grid section challenge

---
+++ vite-grid-section/src/App.jsx
@@ -0,0 +1,130 @@
+function App() {
+  return (
+    <div>
+      <main className="container">
+        <div className="card item1">
+          <div className="card-img">
+            <img src="/image-daniel.jpg" alt="daniel" />
+            <p>
+              Daniel Clifford <span>Verified Graduate</span>
+            </p>
+          </div>
+          <section className="card-text">
+            <p className="text-testimonial">
+              I received a job offer mid-course, and the subjects I learned were
+              current, if not more so, in the company I joined. I honestly feel
+              I got every penny’s worth.
+            </p>
+            <p className="text-quote">
+              “ I was an EMT for many years before I joined the bootcamp. I’ve
+              been looking to make a transition and have heard some people who
+              had an amazing experience here. I signed up for the free intro
+              course and found it incredibly fun! I enrolled shortly thereafter.
+              The next 12 weeks was the best - and most grueling - time of my
+              life. Since completing the course, I’ve successfully switched
+              careers, working as a Software Engineer at a VR startup. ”
+            </p>
+          </section>
+        </div>
+        <div className="card item2">
+          <div className="card-img">
+            <img src="/image-daniel.jpg" alt="daniel" />
+            <p>
+              Daniel Clifford <span>Verified Graduate</span>
+            </p>
+          </div>
+          <section className="card-text">
+            <p className="text-testimonial">
+              I received a job offer mid-course, and the subjects I learned were
+              current, if not more so, in the company I joined. I honestly feel
+              I got every penny’s worth.
+            </p>
+            <p className="text-quote">
+              “ I was an EMT for many years before I joined the bootcamp. I’ve
+              been looking to make a transition and have heard some people who
+              had an amazing experience here. I signed up for the free intro
+              course and found it incredibly fun! I enrolled shortly thereafter.
+              The next 12 weeks was the best - and most grueling - time of my
+              life. Since completing the course, I’ve successfully switched
+              careers, working as a Software Engineer at a VR startup. ”
+            </p>
+          </section>
+        </div>
+        <div className="card item3">
+          <div className="card-img">
+            <img src="/image-daniel.jpg" alt="daniel" />
+            <p>
+              Daniel Clifford <span>Verified Graduate</span>
+            </p>
+          </div>
+          <section className="card-text">
+            <p className="text-testimonial">
+              I received a job offer mid-course, and the subjects I learned were
+              current, if not more so, in the company I joined. I honestly feel
+              I got every penny’s worth.
+            </p>
+            <p className="text-quote">
+              “ I was an EMT for many years before I joined the bootcamp. I’ve
+              been looking to make a transition and have heard some people who
+              had an amazing experience here. I signed up for the free intro
+              course and found it incredibly fun! I enrolled shortly thereafter.
+              The next 12 weeks was the best - and most grueling - time of my
+              life. Since completing the course, I’ve successfully switched
+              careers, working as a Software Engineer at a VR startup. ”
+            </p>
+          </section>
+        </div>
+        <div className="card item4">
+          <div className="card-img">
+            <img src="/image-daniel.jpg" alt="daniel" />
+            <p>
+              Daniel Clifford <span>Verified Graduate</span>
+            </p>
+          </div>
+          <section className="card-text">
+            <p className="text-testimonial">
+              I received a job offer mid-course, and the subjects I learned were
+              current, if not more so, in the company I joined. I honestly feel
+              I got every penny’s worth.
+            </p>
+            <p className="text-quote">
+              “ I was an EMT for many years before I joined the bootcamp. I’ve
+              been looking to make a transition and have heard some people who
+              had an amazing experience here. I signed up for the free intro
+              course and found it incredibly fun! I enrolled shortly thereafter.
+              The next 12 weeks was the best - and most grueling - time of my
+              life. Since completing the course, I’ve successfully switched
+              careers, working as a Software Engineer at a VR startup. ”
+            </p>
+          </section>
+        </div>
+        <div className="card item5">
+          <div className="card-img">
+            <img src="/image-daniel.jpg" alt="daniel" />
+            <p>
+              Daniel Clifford <span>Verified Graduate</span>
+            </p>
+          </div>
+          <section className="card-text">
+            <p className="text-testimonial">
+              I received a job offer mid-course, and the subjects I learned were
+              current, if not more so, in the company I joined. I honestly feel
+              I got every penny’s worth.
+            </p>
+            <p className="text-quote">
+              “ I was an EMT for many years before I joined the bootcamp. I’ve
+              been looking to make a transition and have heard some people who
+              had an amazing experience here. I signed up for the free intro
+              course and found it incredibly fun! I enrolled shortly thereafter.
+              The next 12 weeks was the best - and most grueling - time of my
+              life. Since completing the course, I’ve successfully switched
+              careers, working as a Software Engineer at a VR startup. ”
+            </p>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
