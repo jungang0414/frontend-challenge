8a163a4caf5add1f73a37a4bf224824f7c91db2a
jungang0414 <dayr88520@gmail.com>
2024-09-23 16:56:17 +0800

    feat: update html add level-one heading & update section h2-h6

--- vite-grid-section/src/App.jsx
+++ vite-grid-section/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
       <main className="container">
         <div className="card daniel">
           <div className="card-img">
-            <img src="/image-daniel.jpg" alt="daniel" />
+            <h1><img src="/image-daniel.jpg" alt="daniel" /></h1>
             <p>
               Daniel Clifford <span>Verified Graduate</span>
             </p>
@@ -15,12 +15,12 @@ function App() {
               alt="bg-pattern"
             />
           <section className="card-text">
-            <p className="text-testimonial">
+            <h2 className="text-testimonial">
               I received a job offer mid-course, and the subjects I learned were
               current, if not more so, in the company I joined. I honestly feel
               I got every penny’s worth.
-            </p>
-            <p className="text-quote">
+            </h2>
+            <h3 className="text-quote">
               “ I was an EMT for many years before I joined the bootcamp. I’ve
               been looking to make a transition and have heard some people who
               had an amazing experience here. I signed up for the free intro
@@ -28,7 +28,7 @@ function App() {
               The next 12 weeks was the best - and most grueling - time of my
               life. Since completing the course, I’ve successfully switched
               careers, working as a Software Engineer at a VR startup. ”
-            </p>
+            </h3>
           </section>
         </div>
         <div className="card jonathan">
@@ -39,14 +39,14 @@ function App() {
             </p>
           </div>
           <section className="card-text">
-            <p className="text-testimonial">
+            <h2 className="text-testimonial">
               The team was very supportive and kept me motivated
-            </p>
-            <p className="text-quote">
+            </h2>
+            <h3 className="text-quote">
               “ I started as a total newbie with virtually no coding skills. I
               now work as a mobile engineer for a big company. This was one of
               the best investments I’ve made in myself. ”
-            </p>
+            </h3>
           </section>
         </div>
         <div className="card jeanette">
@@ -57,14 +57,14 @@ function App() {
             </p>
           </div>
           <section className="card-text">
-            <p className="text-testimonial">
+            <h2 className="text-testimonial">
               An overall wonderful and rewarding experience
-            </p>
-            <p className="text-quote">
+            </h2>
+            <h3 className="text-quote">
               “ Thank you for the wonderful experience! I now have a job I
               really enjoy, and make a good living while doing something I love.
               ”
-            </p>
+            </h3>
           </section>
         </div>
         <div className="card patrick">
@@ -75,19 +75,19 @@ function App() {
             </p>
           </div>
           <section className="card-text">
-            <p className="text-testimonial">
+            <h2 className="text-testimonial">
               Awesome teaching support from TAs who did the bootcamp themselves.
               Getting guidance from them and learning from their experiences was
               easy.
-            </p>
-            <p className="text-quote">
+            </h2>
+            <h3 className="text-quote">
               “ The staff seem genuinely concerned about my progress which I
               find really refreshing. The program gave me the confidence
               necessary to be able to go out in the world and present myself as
               a capable junior developer. The standard is above the rest. You
               will get the personal attention you need from an incredible
               community of smart and amazing people. ”
-            </p>
+            </h3>
           </section>
         </div>
         <div className="card kira">
@@ -98,10 +98,10 @@ function App() {
             </p>
           </div>
           <section className="card-text">
-            <p className="text-testimonial">
+            <h2 className="text-testimonial">
               Such a life-changing experience. Highly recommended!
-            </p>
-            <p className="text-quote">
+            </h2>
+            <h3 className="text-quote">
               “ Before joining the bootcamp, I’ve never written a line of code.
               I needed some structure from professionals who can help me learn
               programming step by step. I was encouraged to enroll by a former
@@ -114,7 +114,7 @@ function App() {
               example of my developent experience. It certainly helped me land a
               job as a full-stack developer after receiving multiple offers.
               100% recommend! ”
-            </p>
+            </h3>
           </section>
         </div>
       </main>
